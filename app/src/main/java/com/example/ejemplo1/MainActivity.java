d35f488c1d90c38f6d2d82e833c2ebdcbffb4997
ChinitoPonte <robertogalicianlol@gmail.com>
2020-01-22 14:22:10 +0100

    actualizar al proyecto de el directorio al que tenia del ejemplo 1

---
+++ app/src/main/java/com/example/ejemplo1/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.ejemplo1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView texto;
+    @Nullable
+    private Bundle savedInstanceState;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+            }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("MainActivity", "Estoy en el onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("MainActivity", "Estoy en el onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("MainActivity", "Estoy en el onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("MainActivity", "Estoy en el onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("MainActivity", "Estoy en el onDestroy");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Log.d("MainActivity", "Se ha pulsado el boton hacia atras"); //esto hace que aparezca el msg en el logcat
+
+        Toast toast = Toast.makeText(this, "Anda a tomar por el culo maricon", Toast.LENGTH_SHORT); //aparece el mensaje cuando realizas la accion
+        toast.show(); //esto es para mostrar el mensaje
+    }
+
+
+
+
+
+    public void cambiarActivityRegistro(View v){
+        Intent intent = new Intent (this, RegistroActivity.class);
+        startActivity(intent);
+    }
+
+    public void cambiarActivityMain(View v){
+        Intent intent = new Intent (this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void mostrarContraseña(View v){
+      View view =  findViewById(R.id.etintContraseña);
+        view.findViewById(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+    }
+
+
+
+
+
+
+    public void cambiarMensaje(View v) {
+        SimpleDateFormat formato = new SimpleDateFormat("HH:mm");
+        Date fechaActual = Calendar.getInstance().getTime();
+
+
+        String s = formato.format(fechaActual);
+        texto.setText(String.format("Hora de inicio de sesión: %s", s));
+
+
+    }
+
+
+
+
+
+//
+//
+//    public void loginlol (View v) {
+//
+//
+//        String url = "https://euw.leagueoflegends.com/es/";
+//
+//        Intent i = new Intent(Intent.ACTION_VIEW);
+//        i.setData(Uri.parse(url));
+//        startActivity(i);
+//    }
+//
+//    public void crearcuenta (View v){
+//
+//        String url = "https://signup.euw.leagueoflegends.com/es/signup/index#/";
+//
+//        Intent i = new Intent(Intent.ACTION_VIEW);
+//        i.setData(Uri.parse(url));
+//        startActivity(i);
+//    }
+//
+    public void recuperarcontraseña (View v){
+
+        String url = "https://recovery.riotgames.com/es/forgot-password?region=EUW1";
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+
+
+    }
+
+
+//        public boolean onTouchEvent (View v, MotionEvent event){
+//            EditText password = (EditText) findViewById(R.id.etintContraseña);
+//            switch (event.getAction()) {
+//               case MotionEvent.ACTION_DOWN:
+//                    password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+//                    break;
+//                case MotionEvent.ACTION_BUTTON_PRESS:
+//                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+//                    break;
+//            }
+//            return super.onTouchEvent(event);
+//        }
+//   }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/ejemplo1/RegistroActivity.java
@@ -0,0 +1,42 @@
+package com.example.ejemplo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        Button butRegistro = findViewById(R.id.btRegistro);
+        butRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegistroActivity.this, "Boton pulsado", Toast.LENGTH_SHORT).show();
+
+
+
+
+            }
+        });
+
+
+    }
+
+    public void cambiarActivityMain(View view) {
+            Intent intent = new Intent (this, MainActivity.class);
+            startActivity(intent);
+    }
+
+
+
+
+
+}
